86a0bbf4483fe887822472d9839162777cc20e77
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2024-01-23 10:18:56 +0100

    finalising

--- src/contexts/AppReducer.ts
+++ src/contexts/AppReducer.ts
@@ -4,18 +4,21 @@ export const initialState = {
   employees: mockedEmployees,
 };
 
-export const AppReducer = (state, action) => {
+export const AppReducer = (state: any, action: any) => {
   switch (action.type) {
     case "ADD_EMPLOYEE":
+      // Add new employee to the list of employees
       let newEmployee = [...state.employees, action.payload];
+      // returns the new state of the app, by copying the old state and adding the new employee to the list
       return {
         ...state,
-        employee: newEmployee,
+        employees: newEmployee,
       };
     case "SET_EMPLOYEES":
+      // Sets the list of employees
       return {
         ...state,
-        newEmployee: action.payload,
+        employees: action.payload,
       };
     default:
       return state;
